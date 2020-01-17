1e561417af5b8c360c4a91627e134d561889aa4f
wolverine4582 <59991548+wolverine4582@users.noreply.github.com>
2020-01-17 11:29:02 +0530

    Added the python file

---
+++ GitTest.py
@@ -0,0 +1,10 @@
+print("This is the first file to be committed")
+print("I am Iron Man")
+print("I am Hulk")
+print("I am Thor")
+print("I am Cap")
+print("I am Thanos")
+print("I am Vision")
+print("I am Scarlett witch")
+
+
